fea7e842c8ad9183213f474278275144ba002452
Angelina Morozova <angmorozova@yandex.ru>
2014-11-10 15:37:44 +0400

    Добавила валидацию на стороне сервера

--- index.php
+++ index.php
@@ -8,28 +8,43 @@
     $gender = $_POST['gender'];
     $is_smoke = (isset($_POST['is_smoke']))? 'Курю' : 'Не имею';
 
-    echo <<<EOT
-    <div class='info_table'>
-        <table class='table table-hover'>
-            <tr>
-            <td>ФИО</td><td>$fio</td>
-            </tr>
-            <tr>
-            <td>E-mail</td><td>$email</td>
-            </tr>
-            <tr>
-            <td>Возраст</td><td>$age</td>
-            </tr>
-            <tr>
-            <td>Пол</td><td>$gender</td>
-            </tr>
-            <tr>
-            <td>Вредные привычки</td><td>$is_smoke</td>
-            </tr>
-        </table>
-        <input type='button' onClick="window.location.href=window.location.href" class='btn next btn-success  pull-right' value='Ввести еще раз' />
-    </div>
+    $compare_fio = preg_match('/([А-Яа-я ]+)/u',$fio);
+    $compare_email = preg_match('/(.+@.+\..+)/u',$email);
+
+    if(isset($fio) and isset($email)){
+        if($compare_fio==1 and $compare_email==1){
+            echo <<<EOT
+            <div class='info_table'>
+                <table class='table table-hover'>
+                    <tr>
+                    <td>ФИО</td><td>$fio</td>
+                    </tr>
+                    <tr>
+                    <td>E-mail</td><td>$email</td>
+                    </tr>
+                    <tr>
+                    <td>Возраст</td><td>$age</td>
+                    </tr>
+                    <tr>
+                    <td>Пол</td><td>$gender</td>
+                    </tr>
+                    <tr>
+                    <td>Вредные привычки</td><td>$is_smoke</td>
+                    </tr>
+                </table>
+                <input type='button' onClick="window.location.href=window.location.href" class='btn next btn-success  pull-right' value='Ввести еще раз' />
+            </div>
 EOT;
+        }else{
+            echo "<div class='info_table'><div class='alert alert-danger alert-dismissible' style='width: 350px;' role='alert'>
+             Проверьте правильность ввода данных!
+         </div></div>";
+        }
+    }else{
+        echo "<div class='info_table'><div class='alert alert-danger alert-dismissible' style='width: 350px;' role='alert'>
+             Поля 'Фамилия Имя' и 'Email' обязательны для заполнения
+         </div></div>";
+    }
   }
   include ('footer.html');
 
